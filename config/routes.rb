e8f5bdd948906bd34bd7d743d3a032b98fa0d0e9
Sonukr <sonuyadav9756@gmail.com>
2017-05-03 22:37:17 +0530

    default routes changes to contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :contacts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "contacts#index"
+
+
 end
